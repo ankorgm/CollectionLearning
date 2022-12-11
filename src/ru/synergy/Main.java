f6586ccaf86c148a878b36120e0c1d728c13eec6
ankorgm <ankorgm@gmail.com>
2022-12-12 01:58:48 +0300

    ArrayList example with cats completed

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -18,12 +18,31 @@ public class Main {
         System.out.println(Arrays.toString(cats));
 
         ArrayList<Cat> catslist = new ArrayList<>();
-        for (Cat cat : cats){
+        for (Cat cat : cats) {
             catslist.add(cat);
         }
 
         catslist.add(new Cat("Барсик"));
+
+        catslist.remove(1);
+
+        Cat cat = catslist.get(0);
+
+        System.out.println(cat);
+
+        System.out.println(catslist.indexOf(cat));
+
+        catslist.add(2, cat);
+        catslist.set(2, new Cat("Меня сюда вставили"));
+
+        catslist.removeAll(Arrays.asList(cat, catslist.get(3)));
+
+
         System.out.println(catslist.toString());
+
+        System.out.println(catslist.size());
+
+
 
     }
 
