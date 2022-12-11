b21bdae6df91c4a5b76f404ac9544a9f3fc7e8c3
ankorgm <ankorgm@gmail.com>
2022-12-12 01:24:23 +0300

    Cats example

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,7 +1,19 @@
 package ru.synergy;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Tomas");
+        cats[1] = new Cat("Бегемот");
+        cats[2] = new Cat("Филлип Маркович");
+
+        cats[1] = null;
+
+
+
+        System.out.println(Arrays.toString(cats));
     }
 }
